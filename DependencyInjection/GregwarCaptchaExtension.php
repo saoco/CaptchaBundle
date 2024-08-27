e91b2fa73f44a08cad02a712852e29e8369eb0c5
saoco <chab@gmx.de>
2024-08-27 22:27:58 +0200

    Remove dependency on parameter "twig.form.resources"

--- DependencyInjection/GregwarCaptchaExtension.php
+++ DependencyInjection/GregwarCaptchaExtension.php
@@ -38,7 +38,7 @@ class GregwarCaptchaExtension extends Extension
         $container->setParameter('gregwar_captcha.config.whitelist_key', $config['whitelist_key']);
 
         /** @var array<string> $resources */
-        $resources = $container->getParameter('twig.form.resources');
-        $container->setParameter('twig.form.resources', array_merge(array('@GregwarCaptcha/captcha.html.twig'), $resources));
+        // $resources = $container->getParameter('twig.form.resources');
+        // $container->setParameter('twig.form.resources', array_merge(array('@GregwarCaptcha/captcha.html.twig'), $resources));
     }
 }
